aca70e800d6a996e9eae7e86b8f78dfee5e82ef5
Abel Mendonça <abel.ncm@gmail.com>
2022-11-24 13:35:06 -0100

    entities

--- src/persons/persons.controller.ts
+++ src/persons/persons.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from "@nestjs/common";
-import { Cat } from "./cat.model";
-import { Person } from "./person.model";
+import { Cat } from "./cat.entity";
+import { Person } from "./person.entity";
 import { PersonsService } from "./persons.service";
 
 
